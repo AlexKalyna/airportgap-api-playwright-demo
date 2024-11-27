1e7f8a954be7cbf7b893024487bcc570bbde6edc
AlexKalyna <oleksandkalyna@yahoo.com>
2024-11-27 22:39:28 +0200

    Implemented Scenario P.5.1: Returns all the favorite airports saved to your Airport Gap account.

--- tests/api/authRequired/get.api.ts
+++ tests/api/authRequired/get.api.ts
@@ -1,37 +1,47 @@
-// import { expect, test } from '@playwright/test';
-// import APIClient from '../../../src/client/APIClient';
-// import { getData } from '../../../src/data/dict/userData';
-// import AuthController from '../../../src/controllers/AuthController';
+import { expect, test } from '@playwright/test';
+import APIClient from '../../../src/client/APIClient';
+import { getData } from '../../../src/data/dict/userData';
+import Joi from 'joi';
+import * as schema from 'src/constants/apiResponseSchemas/getResponseSchemas';
+import faker from 'faker';
+import * as helper from 'src/helpers/helpers';
+import { airportIDs } from 'src/data/airports';
 
-// let client: APIClient;
+let client: APIClient;
 
-// test.beforeAll(async () => {
-//   const authData = getData();
-//   client = await APIClient.authenticate(authData);
-// });
+test.beforeAll(async () => {
+  const authData = getData();
+  client = await APIClient.authenticate(authData);
+});
 
-// test.describe('API GET', () => {
-//   test.describe('Positive tests', () => {
-//     test('Add favorite airport @smoke @regression', async () => {
-//       const favoriteAirportData = {
-//         airport_id: 'MAG',
-//         notes: 'Random note'
-//       };
-//       const response = await client.userAirports.addFavoriteAirport(favoriteAirportData);
+test.describe('API GET/favorites', () => {
+  test.describe('Positive tests', () => {
+    test(
+      'Returns all the favorite airports saved to your Airport Gap account.',
+      {
+        tag: ['@P.5.1', '@smoke', '@regression']
+      },
+      async () => {
+        // Adding airport to favorites to make sure the list is not empty
+        const airportID = helper.getRandomAirportID(airportIDs);
+        const note: string = faker.lorem.words();
+        const requestBody = { airport_id: airportID, note: note };
+        const postResponse = await client.userAirports.addAirportToFavorites(requestBody);
+        expect(postResponse.status).toBe(201);
+        // Main test
+        const response = await client.userAirports.getFavouriteAirports();
+        expect(response.status).toBe(200);
+        expect(response.statusText).toBe('OK');
+        Joi.assert(await response.data, Joi.object(schema.GET_FAVORITE_AIRPORTS_SCHEMA));
+      }
+    );
+    //   });
 
-//       // console.log(response);
+    //   test('Return the first page of airports in the Airport Gap database @smoke @regression', async () => {
+    //     const response = await client.userAirports.getUserAirports();
 
-//       expect(response.status).toBe(200);
-//       expect(response.statusText).toBe('OK');
-//       Joi.assert(await response.data, Joi.object(schema.START_CREATED_CALL_SCHEMA));
-//     });
-//   });
-
-//   test('Return the first page of airports in the Airport Gap database @smoke @regression', async () => {
-//     const response = await client.userAirports.getUserAirports();
-
-//     expect(response.status).toBe(200);
-//     expect(response.statusText).toBe('OK');
-//     // Joi.assert(await response.data, Joi.object(schema.START_CREATED_CALL_SCHEMA));
-//   });
-// });
+    //     expect(response.status).toBe(200);
+    //     expect(response.statusText).toBe('OK');
+    //     // Joi.assert(await response.data, Joi.object(schema.START_CREATED_CALL_SCHEMA));
+  });
+});
