e95c84af4e2ab25dec5ad63d4a415855527ea73f
Oleksandr Kalyna <oleksandrkalyna@yahoo.com>
2024-11-28 23:07:18 +0200

    Implemented P.8.1: Update a favorite item with valid data. (#13)
    Co-authored-by: AlexKalyna <oleksandkalyna@yahoo.com>

--- tests/api/authRequired/patch.api.ts
+++ tests/api/authRequired/patch.api.ts
@@ -1,37 +1,47 @@
 import { expect, test } from '@playwright/test';
 import APIClient from '../../../src/client/APIClient';
 import { getData } from '../../../src/data/dict/userData';
-import AuthController from '../../../src/controllers/AuthController';
+import Joi from 'joi';
+import * as schema from 'src/constants/apiResponseSchemas/patchResponseSchemas';
+import faker from 'faker';
+import * as helper from 'src/helpers/helpers';
+import { airportIDs } from 'src/data/airports';
 
-// let client: APIClient;
+let client: APIClient;
 
-// test.beforeAll(async () => {
-//   const authData = getData();
-//   client = await APIClient.authenticate(authData);
-// });
+test.beforeAll(async () => {
+  const authData = getData();
+  client = await APIClient.authenticate(authData);
+});
 
-// test.describe('API GET', () => {
-//   test.describe('Positive tests', () => {
-//     test('Add favorite airport @smoke @regression', async () => {
-//       const favoriteAirportData = {
-//         airport_id: 'MAG',
-//         notes: 'Random note'
-//       };
-//       const response = await client.userAirports.addFavoriteAirport(favoriteAirportData);
+test.afterAll(async () => {
+  await client.userAirports.removeAllAirportsFromFavorites();
+});
 
-//       // console.log(response);
+test.describe('API PATCH/favorites/:id', () => {
+  test.describe('Positive tests', () => {
+    test(
+      'Update a favorite item with valid data.',
+      {
+        tag: ['@P.8.1', '@smoke', '@regression']
+      },
+      async () => {
+        // Adding airport to favorites to make sure the list is not empty
+        const airportID = helper.getRandomAirportID(airportIDs);
+        const note: string = faker.lorem.words();
+        const postRequestBody = { airport_id: airportID, note: note };
+        const postResponse = await client.userAirports.addAirportToFavorites(postRequestBody);
+        expect(postResponse.status).toBe(201);
+        const airoportNumericID = postResponse.data.data.id;
 
-//       expect(response.status).toBe(200);
-//       expect(response.statusText).toBe('OK');
-//       // Joi.assert(await response.data, Joi.object(schema.START_CREATED_CALL_SCHEMA));
-//     });
-//   });
-
-//   test('Return the first page of airports in the Airport Gap database @smoke @regression', async () => {
-//     const response = await client.userAirports.getUserAirports();
-
-//     expect(response.status).toBe(200);
-//     expect(response.statusText).toBe('OK');
-//     // Joi.assert(await response.data, Joi.object(schema.START_CREATED_CALL_SCHEMA));
-//   });
-// });
+        // Main test
+        const requestBody = { note: faker.lorem.words() };
+        const response = await client.userAirports.updateFavoriteAirport(airoportNumericID, requestBody);
+        expect(response.status).toBe(200);
+        expect(response.statusText).toBe('OK');
+        Joi.assert(await response.data, Joi.object(schema.PATCH_FAVORITE_AIRPORT_BY_ID_SCHEMA));
+        expect(response.data.data.attributes.note).toBe(requestBody.note);
+      }
+    );
+  });
+});
